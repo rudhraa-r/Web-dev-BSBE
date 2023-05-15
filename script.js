3d22045f5f5d84213cf5b322721525983146a5ba
rudhraa_r <111883798+rudhraa-r@users.noreply.github.com>
2023-05-15 20:52:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
+
 fetch('studentsInfo.json')
     .then(response => response.json())
     .then(data => {
         const students = data.bsbeY20;
         const list = document.getElementById('slist');
         students.forEach(student => {
-            const li = document.createElement('li');
+            const li = document.createElement('div');
             li.classList.add('card');
             const img = document.createElement('img');
-            const rn = student.rollNo;
+            img.classList.add('dp');
+            const rn = document.createElement('span');
+            rn.classList.add('rn');
+            rn.textContent=`Roll No:${student.rollNo}`;
             img.src = `https://oa.cc.iitk.ac.in/Oa/Jsp/Photo/${student.rollNo}_0.jpg`
             const name = document.createElement('span');
-            name.textContent = student.name;
+            name.classList.add('info');
+            name.textContent = ` Name:${student.name}\n`;
 
             li.appendChild(img);
             li.appendChild(name);
+            li.appendChild(rn);
             list.appendChild(li);
+            
+            
+           /* img.addEventListener('click', function my(){
+				img.classList.add('active');
+			});
+            img.removeEventListener('click',my);*/
 
-            function newFunction() {
-                img.onclick = enlargeImg();
-            }
+           
         });
     })
     /*function enlargeImg() {
